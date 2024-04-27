32063c0932216f20b54bd56ca6d3df906fae2740
SavindiRashmika <rashmikawadanambi>
2024-04-27 22:39:36 +0530

    application propertie file added

---
+++ backEnd/backEnd/src/main/java/lk/ijse/gdse66/backEnd/BackEndApplication.java
@@ -0,0 +1,13 @@
+package lk.ijse.gdse66.backEnd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndApplication.class, args);
+	}
+
+}
