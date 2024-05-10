d5e235901f6d016e7ebf4ef40bbffcf4853d2af8
SavindiRashmika <rashmikawadanambi>
2024-05-10 23:57:07 +0530

    updated

--- FrontEnd/controller/customer.js
+++ FrontEnd/controller/customer.js
@@ -29,4 +29,76 @@ toggler.addEventListener('change', function () {
     }
 });
 
+document.addEventListener("DOMContentLoaded", function() {
+    const inputs = document.querySelectorAll('input[type="text"], input[type="email"],input[type="date"], select');
 
+    inputs.forEach(input => {
+        input.addEventListener('click', function() {
+            // Get the table body element
+            const tableBody = document.querySelector('tbody');
+            // Change its background color
+            tableBody.style.backgroundColor = 'var(--gray)';
+            this.style.backgroundColor = 'var(--gray)';
+            this.style.color = 'var(--dark)';
+        });
+
+        input.addEventListener('blur', function() {
+            // Reset the background color of the table body
+            const tableBody = document.querySelector('tbody');
+            tableBody.style.backgroundColor = '#383838';
+            this.style.backgroundColor = 'var(--gray)';
+            this.style.border = '1px solid #383838';
+
+            // Custom styling for select box options
+           /* const selectOptions = this.querySelectorAll('option');
+            selectOptions.forEach(option => {
+                option.style.backgroundColor = 'black'; // Change background color to black
+                option.style.color = 'var(--dark)';
+*/
+
+                const selectOptions = document.querySelectorAll('select option');
+                selectOptions.forEach(option => {
+                    option.style.backgroundColor = '#333';
+                    option.style.color = 'var(--dark)';
+
+        });
+    });
+    });
+});
+
+
+
+
+/*------------------------------------------------------------*/
+startTime();
+function startTime() {
+    var today = new Date();
+    var hr = today.getHours();
+    var min = today.getMinutes();
+    var sec = today.getSeconds();
+    ap = (hr < 12) ? "<span>AM</span>" : "<span>PM</span>";
+    hr = (hr == 0) ? 12 : hr;
+    hr = (hr > 12) ? hr - 12 : hr;
+    //Add a zero in front of numbers<10
+    hr = checkTime(hr);
+    min = checkTime(min);
+    sec = checkTime(sec);
+    document.getElementById("clock").innerHTML = hr + ":" + min + ":" + sec + " " + ap;
+
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+    var curWeekDay = days[today.getDay()];
+    var curDay = today.getDate();
+    var curMonth = months[today.getMonth()];
+    var curYear = today.getFullYear();
+    var date = curWeekDay+", "+curDay+" "+curMonth+" "+curYear;
+    document.getElementById("date").innerHTML = date;
+
+    var time = setTimeout(function(){ startTime() }, 500);
+}
+function checkTime(i) {
+    if (i < 10) {
+        i = "0" + i;
+    }
+    return i;
+}
